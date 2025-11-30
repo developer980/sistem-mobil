3bdc421c4726cb9308185f31b62f25e7f8420067
Tudor Cernat <91996303+developer980@users.noreply.github.com>
2025-11-30 13:32:55 +0200

    minor adjustments

--- gpio.c
+++ gpio.c
@@ -9,10 +9,12 @@
 #include "driverlib.h"
 #include <adc14.h>
 
+// Configure timer A registers 3 and 4 for PWM signals for motor controls
+
 Timer_A_PWMConfig pwmConfigLeft = {
     TIMER_A_CLOCKSOURCE_SMCLK, // set timer source as SMCLK
     TIMER_A_CLOCKSOURCE_DIVIDER_1,
-    6000,
+    4000,
     TIMER_A_CAPTURECOMPARE_REGISTER_3,
     TIMER_A_OUTPUTMODE_RESET_SET,
     0
@@ -21,16 +23,15 @@ Timer_A_PWMConfig pwmConfigLeft = {
 Timer_A_PWMConfig pwmConfigRight = {
     TIMER_A_CLOCKSOURCE_SMCLK, // set timer source as SMCLK
     TIMER_A_CLOCKSOURCE_DIVIDER_1,
-    6000,
+    4000,
     TIMER_A_CAPTURECOMPARE_REGISTER_4,
     TIMER_A_OUTPUTMODE_RESET_SET,
     0
 };
 
+// Initiate digiral pins
+
 void init_io(){
-
-//    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;          // Stop watchdog timer
-
    // GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
     // Set P1.1 and P1.4 as input (leave direction as input)
     GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0 | GPIO_PIN2 | GPIO_PIN3 | GPIO_PIN5 | GPIO_PIN6 | GPIO_PIN7);
@@ -72,15 +73,20 @@ void init_io(){
 
     GPIO_setAsOutputPin(GPIO_PORT_P6, GPIO_PIN4);
     GPIO_setOutputLowOnPin(GPIO_PORT_P6, GPIO_PIN4);
+
+    GPIO_setAsOutputPin(GPIO_PORT_P6, GPIO_PIN5);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P6, GPIO_PIN5);
 }
 
-
+// Initiate PWM pins
 void init_PWM_pins(){
+
+    // Declare P2.6 and P2.7 for PWM transmitting
     GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN6 | GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
 
+    // Generate PWM signal at the declared pins
     Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfigLeft);
     Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfigRight);
 }
-
 
 
